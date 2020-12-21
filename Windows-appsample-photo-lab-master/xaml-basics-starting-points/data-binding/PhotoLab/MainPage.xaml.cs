4cdf6650338efb19bcf75229c8dc70ad9867ca74
Alex Potasnick <alexander.potasnick@acloud.guru>
2020-12-21 14:13:10 -0500

    Added windows app sample folder

---
+++ Windows-appsample-photo-lab-master/xaml-basics-starting-points/data-binding/PhotoLab/MainPage.xaml.cs
@@ -0,0 +1,159 @@
+﻿//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace PhotoLab
+{
+    public sealed partial class MainPage : Page
+    {
+        public static MainPage Current;
+        private ImageFileInfo persistedItem;
+
+        public ObservableCollection<ImageFileInfo> Images { get; } = new ObservableCollection<ImageFileInfo>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Current = this;
+        }
+
+        // If the image is edited and saved in the details page, this method gets called
+        // so that the back navigation connected animation uses the correct image.
+        public void UpdatePersistedItem(ImageFileInfo item)
+        {
+            persistedItem = item;
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                AppViewBackButtonVisibility.Collapsed;
+
+            if (Images.Count == 0)
+            {
+                await GetItemsAsync();
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        // Called by the Loaded event of the ImageGridView.
+        private async void StartConnectedAnimationForBackNavigation()
+        {
+            // Run the connected animation for navigation back to the main page from the detail page.
+            if (persistedItem != null)
+            {
+                ImageGridView.ScrollIntoView(persistedItem);
+                ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("backAnimation");
+                if (animation != null)
+                {
+                    await ImageGridView.TryStartConnectedAnimationAsync(animation, persistedItem, "ItemImage");
+                }
+            }
+        }
+
+        private async Task GetItemsAsync()
+        {
+            QueryOptions options = new QueryOptions();
+            options.FolderDepth = FolderDepth.Deep;
+            options.FileTypeFilter.Add(".jpg");
+            options.FileTypeFilter.Add(".png");
+            options.FileTypeFilter.Add(".gif");
+
+            // Get the Pictures library. (Requires 'Pictures Library' capability.)
+            //Windows.Storage.StorageFolder picturesFolder = Windows.Storage.KnownFolders.PicturesLibrary;
+            // OR
+            // Get the Sample pictures.
+            StorageFolder appInstalledFolder = Package.Current.InstalledLocation;
+            StorageFolder picturesFolder = await appInstalledFolder.GetFolderAsync("Assets\\Samples");
+
+            var result = picturesFolder.CreateFileQueryWithOptions(options);
+
+            IReadOnlyList<StorageFile> imageFiles = await result.GetFilesAsync();
+            bool unsupportedFilesFound = false;
+            foreach (StorageFile file in imageFiles)
+            {
+                // Only files on the local computer are supported. 
+                // Files on OneDrive or a network location are excluded.
+                if (file.Provider.Id == "computer")
+                {
+                    Images.Add(await LoadImageInfo(file));
+                }
+                else
+                {
+                    unsupportedFilesFound = true;
+                }
+            }
+
+            if (unsupportedFilesFound == true)
+            {
+                ContentDialog unsupportedFilesDialog = new ContentDialog
+                {
+                    Title = "Unsupported images found",
+                    Content = "This sample app only supports images stored locally on the computer. We found files in your library that are stored in OneDrive or another network location. We didn't load those images.",
+                    CloseButtonText = "Ok"
+                };
+
+                ContentDialogResult resultNotUsed = await unsupportedFilesDialog.ShowAsync();
+            }
+
+            // Remove this when replaced with XAML bindings
+            ImageGridView.ItemsSource = Images;
+        }
+
+        public async static Task<ImageFileInfo> LoadImageInfo(StorageFile file)
+        {
+            // Open a stream for the selected file.
+            // The 'using' block ensures the stream is disposed
+            // after the image is loaded.
+            using (IRandomAccessStream fileStream = await file.OpenReadAsync())
+            {
+                // Create a bitmap to be the image source.
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.SetSource(fileStream);
+
+                var properties = await file.Properties.GetImagePropertiesAsync();
+                ImageFileInfo info = new ImageFileInfo(
+                    properties, file, bitmapImage,
+                    file.DisplayName, file.DisplayType);
+
+                return info;
+            }
+        }
+    }
+}
